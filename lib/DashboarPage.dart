f5d92674f2b706fc09b76be2e6ebc8e1f229a64d
Yassed Matta <yassed883@gmail.com>
2023-05-22 14:36:14 -0500

    Creando el archivo de DashboardPage

---
+++ lib/DashboarPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'SettingsPage.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

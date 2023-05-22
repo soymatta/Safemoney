6ac22b27f55514b214b9166cb18b73562f93e405
Yassed Matta <yassed883@gmail.com>
2023-05-22 14:36:44 -0500

    Creando el archivo de SettingsPage

---
+++ lib/SettingsPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'DashboardPage.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

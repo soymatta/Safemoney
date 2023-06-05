170a452235b516fed3e9b64c302b1be8617dca02
Yassed Matta <yassed883@gmail.com>
2023-06-05 04:47:17 -0500

    Modificando pagina de Home

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,6 +4,7 @@ import 'package:safemoney/AddExpenseView.dart';
 import 'package:safemoney/AddIncomeView.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'SettingsView.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = 'Dashboard';
@@ -16,7 +17,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String currency = "Select an option..."; // Valor inicial mientras se carga
-  String Balance = '';
+  String balance = '';
+
+// CODIGO DE GOOGLE NAV
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Widget 1',
+    ),
+    Text(
+      'Widget 2',
+    ),
+  ];
+// CODIGO DE GOOGLE NAV
 
   @override
   void initState() {
@@ -35,7 +48,7 @@ class _HomePageState extends State<HomePage> {
   Future<void> loadBalance() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      Balance = prefs.getString('balance') ?? '';
+      balance = prefs.getString('balance') ?? '';
     });
   }
 
@@ -68,61 +81,203 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Balance: $Balance',
-              style: GoogleFonts.openSans(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                color: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.green,
+                      Color.fromARGB(255, 100, 175, 100),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                height: 200,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Total Balance',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.openSans(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Row(mainAxisSize: MainAxisSize.min, children: [
+                      Text(
+                        '$currency   ',
+                        style: GoogleFonts.openSans(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        balance,
+                        style: GoogleFonts.openSans(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ]),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const AddIncomeView()),
+                              );
+                            },
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Color.fromARGB(255, 135, 255, 135),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  bottomLeft: Radius.circular(10),
+                                ),
+                              ),
+                              height: 70,
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              child: const Center(
+                                child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        'Add income',
+                                        style: TextStyle(fontSize: 15),
+                                      ),
+                                      Icon(
+                                        Icons.arrow_upward,
+                                        color: Colors.green,
+                                        size: 15,
+                                      ),
+                                    ]),
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const AddExpenseView()),
+                              );
+                            },
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Color.fromARGB(255, 135, 255, 135),
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                ),
+                              ),
+                              height: 70,
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              child: const Center(
+                                child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        'Add expense',
+                                        style: TextStyle(fontSize: 15),
+                                      ),
+                                      Icon(
+                                        Icons.arrow_downward,
+                                        color: Colors.red,
+                                        size: 15,
+                                      ),
+                                    ]),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Text(
-              'Currency: $currency', // Usar el valor de currency
-              style: GoogleFonts.openSans(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                color: Colors.black,
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height -
+                      kToolbarHeight -
+                      kBottomNavigationBarHeight -
+                      270,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      _widgetOptions.elementAt(_selectedIndex),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(left: 30.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FloatingActionButton(
-              backgroundColor: Colors.red,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const AddExpenseView(),
-                  ),
-                );
-              },
-              child: const Icon(Icons.attach_money),
-            ),
-            const SizedBox(height: 16),
-            FloatingActionButton(
-              backgroundColor: Colors.green,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const AddIncomeView(),
-                  ),
-                );
-              },
-              child: const Icon(Icons.attach_money),
-            ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
           ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: const Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.black,
+              tabs: const [
+                GButton(
+                  icon: Icons.trending_up,
+                  text: 'Income',
+                ),
+                GButton(
+                  icon: Icons.trending_down,
+                  text: 'Expense',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
         ),
       ),
     );

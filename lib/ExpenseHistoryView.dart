aed7361a3b94d4a4fb69786e29b8d3421b9d2908
Yassed Matta <yassed883@gmail.com>
2023-06-04 23:49:04 -0500

    Limpiando ExpenseHistory e IncomeHistory

--- lib/ExpenseHistoryView.dart
+++ lib/ExpenseHistoryView.dart
@@ -1,25 +1,15 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 
 class Expense {
-  String type;
+  String name;
   double amount;
-  int frequency;
 
   Expense({
-    required this.type,
+    required this.name,
     required this.amount,
-    required this.frequency,
   });
-
-  factory Expense.fromJson(Map<String, dynamic> json) {
-    return Expense(
-      type: json['type'],
-      amount: json['amount'],
-      frequency: json['frequency'],
-    );
-  }
 }
 
 class ExpenseHistoryView extends StatefulWidget {
@@ -30,44 +20,13 @@ class ExpenseHistoryView extends StatefulWidget {
 }
 
 class _ExpenseHistoryViewState extends State<ExpenseHistoryView> {
-  List<Expense> expenses = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadExpenses();
-  }
-
-  void _loadExpenses() async {
-    final prefs = await SharedPreferences.getInstance();
-    final encodedExpenses = prefs.getStringList('expenses');
-
-    if (encodedExpenses != null) {
-      setState(() {
-        expenses = encodedExpenses
-            .map((expense) => Expense.fromJson(jsonDecode(expense)))
-            .toList();
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Expense History'),
       ),
-      body: ListView.builder(
-        itemCount: expenses.length,
-        itemBuilder: (context, index) {
-          final income = expenses[index];
-          return ListTile(
-            title: Text(income.type),
-            subtitle: Text('Amount: ${income.amount.toStringAsFixed(2)}'),
-            trailing: Text('Frequency (Days): ${income.frequency}'),
-          );
-        },
-      ),
+      // body: // TODOS LOS EXPENSE SE DEBEN MOSTRAR EN UNA LISTVIEW
     );
   }
 }
--- lib/IncomeHistoryView.dart
+++ lib/IncomeHistoryView.dart
@@ -1,25 +1,15 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 
 class Income {
-  String type;
+  String name;
   double amount;
-  int frequency;
 
   Income({
-    required this.type,
+    required this.name,
     required this.amount,
-    required this.frequency,
   });
-
-  factory Income.fromJson(Map<String, dynamic> json) {
-    return Income(
-      type: json['type'],
-      amount: json['amount'],
-      frequency: json['frequency'],
-    );
-  }
 }
 
 class IncomeHistoryView extends StatefulWidget {
@@ -30,44 +20,13 @@ class IncomeHistoryView extends StatefulWidget {
 }
 
 class _IncomeHistoryViewState extends State<IncomeHistoryView> {
-  List<Income> incomes = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadIncomes();
-  }
-
-  void _loadIncomes() async {
-    final prefs = await SharedPreferences.getInstance();
-    final encodedIncomes = prefs.getStringList('incomes');
-
-    if (encodedIncomes != null) {
-      setState(() {
-        incomes = encodedIncomes
-            .map((income) => Income.fromJson(jsonDecode(income)))
-            .toList();
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Income History'),
       ),
-      body: ListView.builder(
-        itemCount: incomes.length,
-        itemBuilder: (context, index) {
-          final income = incomes[index];
-          return ListTile(
-            title: Text(income.type),
-            subtitle: Text('Amount: ${income.amount.toStringAsFixed(2)}'),
-            trailing: Text('Frequency (Days): ${income.frequency}'),
-          );
-        },
-      ),
+      // body: // TODOS LOS INCOME SE DEBEN MOSTRAR EN UNA LISTVIEW
     );
   }
 }
